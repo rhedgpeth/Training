1bbeef6b5f76d4b17ccbd2d47909050a16470fc7
Robert Hedgpeth <robert.hedgpeth@xamarin.com>
2016-06-10 17:11:27 -0500

    WeatherService

---
+++ EpocratesTraining/Models/ForecastDay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace EpocratesTraining.Models
+{
+	public class ForecastDay
+	{
+		public int Period { get; set; }
+		public string Icon { get; set; }
+		[JsonProperty("icon_url")]
+		public string IconUrl { get; set; }
+		public string Title { get; set; }
+		[JsonProperty("fcttext")]
+		public string Description { get; set; }
+		[JsonProperty("fcttext_metric")]
+		public string DescriptionMetric { get; set; }
+	}
+}
+
---
+++ EpocratesTraining/Services/WeatherService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EpocratesTraining.Models;
+
+namespace EpocratesTraining.Services
+{
+	public class WeatherService : BaseService
+	{
+		private const string apiKey = "a660601619ff5337";
+
+		static readonly WeatherService instance = new WeatherService();
+
+		public static WeatherService Instance
+		{
+			get { return instance; }
+		}
+
+		WeatherService()
+		{
+			client.BaseAddress = new Uri($"http://api.wunderground.com/api/{apiKey}/");
+		}
+
+		public Task<List<ForecastDay>> Get10DayForecast()
+		{
+			return GetAsync<List<ForecastDay>>("forecast10day/q/CA/San_Francisco.json", x => x["forecast"]["txt_forecast"]["forecastday"]);
+		}
+	}
+}
+
